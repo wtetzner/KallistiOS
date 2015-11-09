439d9e1dca4fd85ed2179863bff02d61a1ca96ed
Lawrence Sebald <ljsebald@users.sourceforge.net>
2015-11-08 20:07:23 -0500

    Add C11 aligned_alloc() function and clean up malloc.h documentation.

--- include/malloc.h
+++ include/malloc.h
@@ -1,7 +1,8 @@
 /* KallistiOS ##version##
 
    malloc.h
-   Copyright (C)2003 Dan Potter
+   Copyright (C) 2003 Dan Potter
+   Copyright (C) 2015 Lawrence Sebald
 
 */
 
@@ -11,6 +12,7 @@
     This implements standard C heap allocation, deallocation, and stats.
 
     \author Dan Potter
+    \author Lawrence Sebald
 */
 
 #ifndef __MALLOC_H
@@ -48,81 +50,144 @@ struct mallinfo {
     int keepcost;
 };
 
-/** \brief allocate memory
+/** \brief  Allocate a block of memory.
 
-    This allocates the specified size of bytes onto the heap. This memory is not
-    freed automatically if the returned pointer goes out of scope. Thus you must
-    call \ref free to reclaim the used space when finished.
+    This function allocates a block of memory of the specified number of bytes
+    on the heap. This memory must later be freed by way of the free() function.
+    The memory *is not* freed simply because a pointer to it goes out of scope.
 
-    \param size is the size in bytes to allocate
-    \return a pointer to the newly allocated address or NULL on errors.
-    \see free
-    \note the memory chunk is uninitialized
-    \note NULL may also be returned if size is 0
+    \param  size            Number of bytes of memory to allocate.
+    \return                 A pointer to the newly allocated block of memory, or
+                            NULL on failure.
+
+    \see                    free
+    \see                    calloc
+    \note                   The block of memory returned is completely
+                            uninitialized and may contain random data.
 */
-void * malloc(size_t size);
+void *malloc(size_t size);
 
-/** \brief allocate memory on the heap and initialize it to 0
+/** \brief  Allocate a block of memory and initialize it to 0.
 
-    This allocates a chunk of memory of size * nmemb. In otherwords, an array
-    with nmemb elements of size or size[nmemb].
+    This function allocates a block of memory of size * nmemb bytes,
+    initializing it to all zero bytes in the process. In other words, this
+    function allocates an array of nmemb elements that are each size bytes in
+    length. Just as with malloc(), you are responsible for calling free() on the
+    block of memory when you are done with it.
 
-    \param nmemb is the amount of elements
-    \param size the size of each element
-    \return a pointer to the newly allocated address or NULL on errors.
-    \see free
-    \note the memory chunk is set to zero
-    \note NULL may be returned if nmemb or size is 0
+    \param  nmemb           Number of elements to allocate space for.
+    \param  size            Size of each element in the array.
+    \return                 A pointer to the newly allocated block of memory, or
+                            NULL on failure.
+
+    \see                    free
+    \see                    malloc
 */
-void * calloc(size_t nmemb, size_t size);
+void *calloc(size_t nmemb, size_t size);
 
-/** \brief releases memory that was previous allocated
+/** \brief  Release a previously allocated block of memory.
 
-    frees the memory space that had previously been allocated by malloc or
-    calloc.
+    This function frees memory that has been previously allocated by way of any
+    of the allocation functions in this file (malloc(), calloc(), memalign(),
+    valloc(), or aligned_alloc()), releasing the memory to be potentially used
+    for any future allocations.
 
-    \param ptr is a pointer to the address of allocated ram
-    \note no action is taken if NULL is passed
-    \note calling free on the same ptr more than once should not be expected to
-        behave in a reproducable maner as it is unpredictable.
+    \param  ptr             A pointer to the block of memory to be freed.
+
+    \note                   Passing a NULL pointer to this function has no
+                            effect.
+    \note                   Attempting to free the same block of memory twice
+                            exhibits undefined behavior (i.e, it may crash, it
+                            may do something else evil, etc).
+    \note                   Attempting to free a block of memory that was not
+                            allocated by way of the normal allocation functions
+                            exhibits undefined behavior.
 */
-void   free(void * ptr);
+void free(void *ptr);
 
-/** \brief changes the size of previously allocated memory
+/** \brief  Change the size of a previously allocated block of memory.
 
-    The size of ptr is changed to size. If data has already been placed in the
-    memory area at that location, it's preserved up to size. If size is larger
-    then the previously allocated memory, the new area will be unititialized.
+    This function changes the size of the previously allocated block of memory
+    from its current size to the specified size. This may involve reallocating
+    and copying the data from the original pointer to a new location in memory.
 
-    \param ptr the address pointer that's been previously returned by malloc/calloc
-    \param size the new size to give to the memory
-    \return a pointer to the new address
-    \note if ptr is NULL the call is basically a malloc(size)
-    \note if ptr is not NULL, and size is 0 the call is basically a free(ptr)
+    If this function returns non-NULL, the old pointer is considered invalid and
+    should not be used (unless, of course, the returned pointer is the same as
+    the old pointer). No action is needed to clean up the old pointer, as this
+    function will have freed it if necessary.
+
+    If this function returns NULL, the old pointer is still valid and has not
+    had its size changed.
+
+    \param  ptr             A pointer to the block of memory to resize. It must
+                            have been previously allocated by way of one of the
+                            memory allocation functions in this file (or must
+                            be NULL).
+    \param  size            The requested size of the new block of memory.
+    \return                 A pointer to the newly allocted/resized block of
+                            memory on success, or NULL on failure.
+
+    \note                   If ptr is NULL on a call to this function, the
+                            function acts the same as malloc(size).
+    \note                   If ptr is non-NULL and size is zero, this function
+                            *does not* free the block of memory -- it simply
+                            returns a block that is of minimal size. By the
+                            standard, this pointer must not be dereferenced.
 */
-void * realloc(void * ptr, size_t size);
+void *realloc(void *ptr, size_t size);
 
-/** \brief allocate memory aligned memory
+/** \brief  Allocate a block of memory aligned to a specified block size.
 
-    Memory of size is allocated with the address being a multiple of alignment
+    This function allocates a block of memory such that the lowest address in
+    the block is aligned to a multiple of alignment bytes. This is useful, for
+    instance, for things like DMA that require aligned blocks of memory to work.
 
-    \param alignment a multiple of two that the memory address will be aligned to
-    \param size the size of the memory to allocate
-    \return a pointer to the newly allocated address (aligned to alignment) or
-        NULL on errors
+    Over-reliance on memalign will most certainly cause memory fragmentation, so
+    you should only use it when it is actually necessary.
+
+    \param  alignment       The alignment requested for the block of memory.
+                            This must be a power-of-two.
+    \param  size            The number of bytes of memory to allocate.
+    \return                 A pointer to the newly allocated block of memory on
+                            success, or NULL on failure.
+
+    \note                   All memory allocation functions will have their
+                            blocks aligned on 8-byte boundaries. There is no
+                            reason to call this function if you need less than
+                            16-byte alignment.
 */
-void * memalign(size_t alignment, size_t size);
+void *memalign(size_t alignment, size_t size);
 
-/** \brief allocates memory aligned to the system page size
+/** \brief  Allocate a block of memory aligned to the system page size.
 
-    Memory is allocated of size and is aligned to the system page size.
-    This ends up basically being: memolign(PAGESIZE, size)
+    This function allocates a block of memory such that the lowest address in
+    the block is aligned to the system page size (typically 4096 bytes). It
+    basically ends up doing return memalign(PAGESIZE, size).
 
-    \param size the size of the memory to allocate
-    \return a pointer to the newly allocated memory address
-    \see arch/arch.h
+    \param  size            The number of bytes of memory to allocate.
+    \return                 A pointer to the newly allocated block of memory on
+                            success, or NULL on failure.
+
+    \see                    PAGESIZE
+    \see                    <arch/arch.h>
 */
-void * valloc(size_t size);
+void *valloc(size_t size);
+
+#if !defined(__STRICT_ANSI__) || (__STDC_VERSION__ >= 201112L)
+
+/** \brief  Allocate memory aligned to a specified block size.
+
+    This function is the standard-compliant C11 method for allocating aligned
+    blocks of memory. It works mostly the same as memalign(), although the
+    size must be a multiple of the alignment.
+
+    \param      alignment   Required alignment of the memory block.
+    \param      size        Number of bytes of memory to allocate.
+    \return                 A pointer to the newly allocated memory block.
+*/
+void *aligned_alloc(size_t alignment, size_t size);
+
+#endif /* !defined(__STRICT_ANSI__) || (__STDC_VERSION__ >= 201112L) */
 
 /** \brief Sets tunable parameters for malloc related options.
 */
@@ -149,7 +214,15 @@ int  mallopt(int, int);
 */
 void malloc_stats();
 
-/** \brief KOS specfic calls
+/** \brief  Determine if it is safe to call malloc() in an IRQ context.
+
+    This function checks the value of the internal spinlock that is used for
+    malloc() to ensure that a call to it will not freeze the running process.
+    This is only really useful in an IRQ context to ensure that a call to
+    malloc() (or some other memory allocation function) won't cause a deadlock.
+
+    \retval     1           If it is safe to call malloc() in the current IRQ.
+    \retval     0           Otherwise.
 */
 int malloc_irq_safe();
 
@@ -164,4 +237,3 @@ int mem_check_all();
 __END_DECLS
 
 #endif  /* __MALLOC_H */
-
