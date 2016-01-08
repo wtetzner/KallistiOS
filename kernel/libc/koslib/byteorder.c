a0b9d7798f1612c9c49f6bd46878bebcbbb09473
Lawrence Sebald <ljsebald@users.sourceforge.net>
2016-01-08 18:30:27 -0500

    Use <arch/byteorder.h> for ntohs/ntohl/htons/htonl.

--- kernel/libc/koslib/byteorder.c
+++ kernel/libc/koslib/byteorder.c
@@ -1,35 +1,31 @@
 /* KallistiOS ##version##
 
    byteorder.c
-   (c)2001 Dan Potter
+   Copyright (C) 2001 Dan Potter
+   Copyright (C) 2016 Lawrence Sebald
 
 */
 
 /* Byte-order translation functions */
-#include <inttypes.h>
-
-/* XXX needs to be in arch */
+#include <stdint.h>
+#include <arch/byteorder.h>
 
 /* Network to Host short */
 uint16_t ntohs(uint16_t value) {
-    return ((value >> 8) & 0xff)
-           | ((value << 8) & 0xff00);
+    return arch_ntohs(value);
 }
 
 /* Network to Host long */
 uint32_t ntohl(uint32_t value) {
-    return ((value >> 24) & 0xff)
-           | (((value >> 16) & 0xff) << 8)
-           | (((value >> 8) & 0xff) << 16)
-           | (((value >> 0) & 0xff) << 24);
+    return arch_ntohl(value);
 }
 
 /* Host to Network short */
 uint32_t htons(uint32_t value) {
-    return ntohs(value);
+    return arch_htons(value);
 }
 
 /* Host to Network long */
 uint32_t htonl(uint32_t value) {
-    return ntohl(value);
+    return arch_htonl(value);
 }
