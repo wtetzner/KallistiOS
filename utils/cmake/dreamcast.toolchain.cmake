c83711685a5c0dcca7a366e4401d25ba8047faba
falco <gyrovorbis@gmail.com>
2023-02-01 06:03:04 -0500

    Added CMake Toolchain file + environment variable
    - Added utils/cmake/dreamcast.toolchain.cmake, a toolchain file
      configured for setting up KOS and cross-compiling for DC with CMake
    - added environment variable to doc/environ.sh.sample for being able to
      conveniently reference the toolchain when passing it to CMake from the
    command-line

---
+++ utils/cmake/dreamcast.toolchain.cmake
@@ -0,0 +1,88 @@
+# CMake Toolchain file for targeting the Dreamcast or NAOMI with CMake.
+#  (c)2023 Falco Girgis, Luke Benstead
+#
+# This file is to be passed to CMake when compiling a regular CMake project
+# to cross-compile for the Dreamcast, using the KOS environment and settings.
+#
+#     cmake /path/to/src -DCMAKE_TOOLCHAIN_FILE=${KOS_BASE}/kos/utils/cmake/dreamcast.toolchain.cmake
+#
+#   or alternatively:
+#
+#     cmake /path/to/src -DCMAKE_TOOLCHAIN_FILE=${KOS_CMAKE_TOOLCHAIN}
+#
+# The toolchain enables the options required for the compiler to use the 
+# special SH4 instructions for sin/cos and invSqrt when in release build
+# and disables them in debug builds. Frame pointers are enabled in debug
+# builds as these are required for stack traces and GDB.
+# 
+# The original toolchain file was created by Kazade for the Simulant 
+# engine who has graciously allowed the rest of the scene to warez it. 
+#
+# Special thanks to Matt Godbolt of Compiler Explorer, for supporting
+# the SH toolchains, which allowed us to figure out the precise magical
+# incantations of compiler flags for fast math.  
+
+##### Configure CMake System #####
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION 1)
+set(PLATFORM_DREAMCAST TRUE)
+
+##### Configure Cross-Compiler #####
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_C_COMPILER $ENV{KOS_CC_BASE}/bin/sh-elf-gcc)
+set(CMAKE_CXX_COMPILER $ENV{KOS_CC_BASE}/bin/sh-elf-g++)
+
+set(CMAKE_EXECUTABLE_SUFFIX ".elf")
+
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+##### Add Platform-Specific #defines #####
+ADD_DEFINITIONS(
+    -D__DREAMCAST__
+    -D_arch_dreamcast
+    -D__arch_dreamcast
+    -D_arch_sub_pristine
+)
+
+##### Configure Build Flags #####
+add_compile_options(-ml -m4-single-only -ffunction-sections -fdata-sections)
+
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -mfsrra -mfsca -ffp-contract=fast -ffast-math -fomit-frame-pointer -g0")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -mfsrra -mfsca -ffp-contract=fast -ffast-math -fomit-frame-pointer -g0")
+
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DFRAME_POINTERS -fno-omit-frame-pointer")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DFRAME_POINTERS -fno-omit-frame-pointer")
+
+set(CMAKE_ASM_FLAGS "")
+set(CMAKE_ASM_FLAGS_RELEASE "")
+
+##### Configure Include Directories #####
+set(CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_SYSTEM_INCLUDE_PATH} $ENV{KOS_BASE}/include $ENV{KOS_BASE}/kernel/arch/dreamcast/include $ENV{KOS_BASE}/addons/include $ENV{KOS_BASE}/../kos-ports/include")
+
+INCLUDE_DIRECTORIES(
+    $ENV{KOS_BASE}/include
+    $ENV{KOS_BASE}/kernel/arch/dreamcast/include
+    $ENV{KOS_BASE}/addons/include
+    $ENV{KOS_BASE}/../kos-ports/include
+)
+
+##### Configure Libraries #####
+set(CMAKE_SYSTEM_LIBRARY_PATH "${CMAKE_SYSTEM_LIBRARY_PATH} $ENV{KOS_BASE}/addons/lib/dreamcast $ENV{KOS_PORTS}/lib")
+
+IF($ENV{KOS_SUBARCH} MATCHES naomi)
+    set(CMAKE_EXE_LINKER_FLAGS " -ml -m4-single-only -Wl,-Ttext=0x8c020000 -Wl,--gc-sections -T$ENV{KOS_BASE}/utils/ldscripts/shlelf-naomi.xc -nodefaultlibs" CACHE INTERNAL "" FORCE)
+ELSE()
+    set(CMAKE_EXE_LINKER_FLAGS " -ml -m4-single-only -Wl,-Ttext=0x8c010000 -Wl,--gc-sections -T$ENV{KOS_BASE}/utils/ldscripts/shlelf.xc -nodefaultlibs" CACHE INTERNAL "" FORCE)
+ENDIF()
+
+LINK_DIRECTORIES(
+    $ENV{KOS_BASE}/addons/lib/dreamcast
+    $ENV{KOS_PORTS}/lib
+)
+
+add_link_options(-L$ENV{KOS_BASE}/lib/dreamcast -L$ENV{KOS_BASE}/addons/lib/dreamcast)
+LINK_LIBRARIES(-Wl,--start-group -lstdc++ -lkallisti -lc -lgcc -Wl,--end-group m)
+LINK_LIBRARIES(c gcc kallisti stdc++)
+
+
