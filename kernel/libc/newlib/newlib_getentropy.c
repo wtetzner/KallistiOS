3c642bf921459112fe559484ab176f73721ab67b
Luke Benstead <kazade@gmail.com>
2023-03-01 09:18:33 +0000

    Update kernel/libc/newlib/newlib_getentropy.c
    Co-authored-by: Lawrence Sebald <ljsebald@users.noreply.github.com>

--- kernel/libc/newlib/newlib_getentropy.c
+++ kernel/libc/newlib/newlib_getentropy.c
@@ -10,26 +10,29 @@
 #include <sys/time.h>
 
 int getentropy(void *ptr, size_t len) {
-    /* We read backwards from the end of available memory
-    and XOR in blocks into the key array, while XORing with the current time.
-    If anyone has a better idea for generating entropy then send a patch :) */
-
+    const int block_size = 128;
     struct timeval tv;
+    uint8_t *src = ((uint8_t *)_arch_mem_top);
+    uint8_t *dst = ptr;
+    size_t i;
+    int j;
+    uint8_t b;
+
     gettimeofday(&tv, NULL);
 
-    const int block_size = 128;
+    /* We read backwards from the end of available memory
+       and XOR in blocks into the key array, while XORing with the current time.
+       If anyone has a better idea for generating entropy then send a patch :) */
+    for(i = 0; i < len; ++i) {
+        b = tv.tv_usec & 0xff;
 
-    uint8_t* src = ((uint8_t*) _arch_mem_top);
-    uint8_t* dst = ptr;
-    for(size_t i = 0; i < len; ++i) {
-        uint8_t b = tv.tv_usec % 255;
-
-        for(int j = 0; j < block_size; ++j) {
+        for(j = 0; j < block_size; ++j) {
             b ^= *--src;
         }
 
         *dst = b;
         ++dst;
     }
+
     return 0;
 }
