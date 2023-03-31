53df25d6fc9ad34e5f8e5adb26934dc80eb1171e
Andress Barajas <andress.barajas@gmail.com>
2023-03-31 16:15:38 -0700

    Added documentation inside the comments

--- kernel/arch/dreamcast/include/arch/timer.h
+++ kernel/arch/dreamcast/include/arch/timer.h
@@ -251,46 +251,54 @@ void timer_shutdown();
 */
 #define PMCR_COUNT_RATIO_CYCLES 1
 
+/** \defgroup   perf_counters Performance Counters
+    This is the list of modes that are allowed to be passed into the perf_cntr_start()
+    function, representing different things you want to count.
+    @{
+*/
 /*                MODE DEFINITION                  VALUE   MEASURMENT TYPE & NOTES */
-#define PMCR_INIT_NO_MODE                           0x00 /* None; Just here to be complete */
-#define PMCR_OPERAND_READ_ACCESS_MODE               0x01 /* Quantity; With cache */
-#define PMCR_OPERAND_WRITE_ACCESS_MODE              0x02 /* Quantity; With cache */
-#define PMCR_UTLB_MISS_MODE                         0x03 /* Quantity */
-#define PMCR_OPERAND_CACHE_READ_MISS_MODE           0x04 /* Quantity */
-#define PMCR_OPERAND_CACHE_WRITE_MISS_MODE          0x05 /* Quantity */
-#define PMCR_INSTRUCTION_FETCH_MODE                 0x06 /* Quantity; With cache */
-#define PMCR_INSTRUCTION_TLB_MISS_MODE              0x07 /* Quantity */
-#define PMCR_INSTRUCTION_CACHE_MISS_MODE            0x08 /* Quantity */
-#define PMCR_ALL_OPERAND_ACCESS_MODE                0x09 /* Quantity */
-#define PMCR_ALL_INSTRUCTION_FETCH_MODE             0x0a /* Quantity */
-#define PMCR_ON_CHIP_RAM_OPERAND_ACCESS_MODE        0x0b /* Quantity */
+#define PMCR_INIT_NO_MODE                           0x00 /**< \brief None; Just here to be complete */
+#define PMCR_OPERAND_READ_ACCESS_MODE               0x01 /**< \brief Quantity; With cache */
+#define PMCR_OPERAND_WRITE_ACCESS_MODE              0x02 /**< \brief Quantity; With cache */
+#define PMCR_UTLB_MISS_MODE                         0x03 /**< \brief Quantity */
+#define PMCR_OPERAND_CACHE_READ_MISS_MODE           0x04 /**< \brief Quantity */
+#define PMCR_OPERAND_CACHE_WRITE_MISS_MODE          0x05 /**< \brief Quantity */
+#define PMCR_INSTRUCTION_FETCH_MODE                 0x06 /**< \brief Quantity; With cache */
+#define PMCR_INSTRUCTION_TLB_MISS_MODE              0x07 /**< \brief Quantity */
+#define PMCR_INSTRUCTION_CACHE_MISS_MODE            0x08 /**< \brief Quantity */
+#define PMCR_ALL_OPERAND_ACCESS_MODE                0x09 /**< \brief Quantity */
+#define PMCR_ALL_INSTRUCTION_FETCH_MODE             0x0a /**< \brief Quantity */
+#define PMCR_ON_CHIP_RAM_OPERAND_ACCESS_MODE        0x0b /**< \brief Quantity */
 /* No 0x0c */
-#define PMCR_ON_CHIP_IO_ACCESS_MODE                 0x0d /* Quantity */
-#define PMCR_OPERAND_ACCESS_MODE                    0x0e /* Quantity; With cache, counts both reads and writes */
-#define PMCR_OPERAND_CACHE_MISS_MODE                0x0f /* Quantity */
-#define PMCR_BRANCH_ISSUED_MODE                     0x10 /* Quantity; Not the same as branch taken! */
-#define PMCR_BRANCH_TAKEN_MODE                      0x11 /* Quantity */
-#define PMCR_SUBROUTINE_ISSUED_MODE                 0x12 /* Quantity; Issued a BSR, BSRF, JSR, JSR/N */
-#define PMCR_INSTRUCTION_ISSUED_MODE                0x13 /* Quantity */
-#define PMCR_PARALLEL_INSTRUCTION_ISSUED_MODE       0x14 /* Quantity */
-#define PMCR_FPU_INSTRUCTION_ISSUED_MODE            0x15 /* Quantity */
-#define PMCR_INTERRUPT_COUNTER_MODE                 0x16 /* Quantity */
-#define PMCR_NMI_COUNTER_MODE                       0x17 /* Quantity */
-#define PMCR_TRAPA_INSTRUCTION_COUNTER_MODE         0x18 /* Quantity */
-#define PMCR_UBC_A_MATCH_MODE                       0x19 /* Quantity */
-#define PMCR_UBC_B_MATCH_MODE                       0x1a /* Quantity */
+#define PMCR_ON_CHIP_IO_ACCESS_MODE                 0x0d /**< \brief Quantity */
+#define PMCR_OPERAND_ACCESS_MODE                    0x0e /**< \brief Quantity; With cache, counts both reads and writes */
+#define PMCR_OPERAND_CACHE_MISS_MODE                0x0f /**< \brief Quantity */
+#define PMCR_BRANCH_ISSUED_MODE                     0x10 /**< \brief Quantity; Not the same as branch taken! */
+#define PMCR_BRANCH_TAKEN_MODE                      0x11 /**< \brief Quantity */
+#define PMCR_SUBROUTINE_ISSUED_MODE                 0x12 /**< \brief Quantity; Issued a BSR, BSRF, JSR, JSR/N */
+#define PMCR_INSTRUCTION_ISSUED_MODE                0x13 /**< \brief Quantity */
+#define PMCR_PARALLEL_INSTRUCTION_ISSUED_MODE       0x14 /**< \brief Quantity */
+#define PMCR_FPU_INSTRUCTION_ISSUED_MODE            0x15 /**< \brief Quantity */
+#define PMCR_INTERRUPT_COUNTER_MODE                 0x16 /**< \brief Quantity */
+#define PMCR_NMI_COUNTER_MODE                       0x17 /**< \brief Quantity */
+#define PMCR_TRAPA_INSTRUCTION_COUNTER_MODE         0x18 /**< \brief Quantity */
+#define PMCR_UBC_A_MATCH_MODE                       0x19 /**< \brief Quantity */
+#define PMCR_UBC_B_MATCH_MODE                       0x1a /**< \brief Quantity */
 /* No 0x1b-0x20 */
-#define PMCR_INSTRUCTION_CACHE_FILL_MODE            0x21 /* Cycles */
-#define PMCR_OPERAND_CACHE_FILL_MODE                0x22 /* Cycles */
-#define PMCR_ELAPSED_TIME_MODE                      0x23 /* Cycles; For 200MHz CPU: 5ns per count in 1 cycle = 1 count mode, or around 417.715ps per count (increments by 12) in CPU/bus ratio mode */
-#define PMCR_PIPELINE_FREEZE_BY_ICACHE_MISS_MODE    0x24 /* Cycles */
-#define PMCR_PIPELINE_FREEZE_BY_DCACHE_MISS_MODE    0x25 /* Cycles */
+#define PMCR_INSTRUCTION_CACHE_FILL_MODE            0x21 /**< \brief Cycles */
+#define PMCR_OPERAND_CACHE_FILL_MODE                0x22 /**< \brief Cycles */
+#define PMCR_ELAPSED_TIME_MODE                      0x23 /**< \brief Cycles; For 200MHz CPU: 5ns per count in 1 cycle = 1 count mode, or around 417.715ps per count (increments by 12) in CPU/bus ratio mode */
+#define PMCR_PIPELINE_FREEZE_BY_ICACHE_MISS_MODE    0x24 /**< \brief Cycles */
+#define PMCR_PIPELINE_FREEZE_BY_DCACHE_MISS_MODE    0x25 /**< \brief Cycles */
 /* No 0x26 */
-#define PMCR_PIPELINE_FREEZE_BY_BRANCH_MODE         0x27 /* Cycles */
-#define PMCR_PIPELINE_FREEZE_BY_CPU_REGISTER_MODE   0x28 /* Cycles */
-#define PMCR_PIPELINE_FREEZE_BY_FPU_MODE            0x29 /* Cycles */
+#define PMCR_PIPELINE_FREEZE_BY_BRANCH_MODE         0x27 /**< \brief Cycles */
+#define PMCR_PIPELINE_FREEZE_BY_CPU_REGISTER_MODE   0x28 /**< \brief Cycles */
+#define PMCR_PIPELINE_FREEZE_BY_FPU_MODE            0x29 /**< \brief Cycles */
+/** @} */
+
 
 /** \brief  Get a performance counter's settings.
+    \ingroup perf_counters
 
     This function returns a performance counter's settings.
 
@@ -300,6 +308,7 @@ void timer_shutdown();
 uint16 perf_cntr_get_config(int which);
 
 /** \brief  Start a performance counter.
+    \ingroup perf_counters
 
     This function starts a performance counter
 
@@ -311,6 +320,7 @@ uint16 perf_cntr_get_config(int which);
 int perf_cntr_start(int which, int mode, int count_type);
 
 /** \brief  Stop a performance counter.
+    \ingroup perf_counters
 
     This function stops a performance counter that was started with perf_cntr_start().
     Stopping a counter retains its count. To clear the count use perf_cntr_clear().
@@ -320,7 +330,8 @@ int perf_cntr_start(int which, int mode, int count_type);
 */
 int perf_cntr_stop(int which);
 
-/** \brief  Clear a performance counter
+/** \brief  Clear a performance counter.
+    \ingroup perf_counters
 
     This function clears a performance counter. It resets its count to zero.
     This function stops the counter before clearing it because you cant clear 
@@ -332,6 +343,7 @@ int perf_cntr_stop(int which);
 int perf_cntr_clear(int which);
 
 /** \brief  Obtain the count of a performance counter.
+    \ingroup perf_counters
 
     This function simply returns the count of the counter.
 
@@ -341,6 +353,7 @@ int perf_cntr_clear(int which);
 uint64 perf_cntr_count(int which);
 
 /** \brief  Enable the nanosecond timer.
+    \ingroup perf_counters
 
     This function enables the performance counter used for the timer_ns_gettime64() 
     function. This is on by default. The function uses \ref PRFC0 to do the work.
@@ -348,6 +361,7 @@ uint64 perf_cntr_count(int which);
 void timer_ns_enable();
 
 /** \brief  Disable the nanosecond timer.
+    \ingroup perf_counters
 
     This function disables the performance counter used for the timer_ns_gettime64() 
     function. Generally, you will not want to do this, unless you have some need to use 
@@ -356,6 +370,7 @@ void timer_ns_enable();
 void timer_ns_disable();
 
 /** \brief  Get the current uptime of the system (in nanoseconds).
+    \ingroup perf_counters
 
     This function retrieves the number of nanoseconds since KOS was started.
 
