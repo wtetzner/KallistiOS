3fc8c74a998438b514fad2a5afed9084f20ae04f
darc <darcagn@protonmail.com>
2023-06-23 21:44:45 -0500

    Only send ARP reply packets if we are intended receiver

--- kernel/net/net_arp.c
+++ kernel/net/net_arp.c
@@ -295,7 +295,9 @@ int net_arp_input(netif_t *nif, const uint8 *pkt_in, int len) {
 
     switch(pkt->opcode[1]) {
         case 1: /* ARP Request */
-            net_arp_send(nif, pkt);
+            /* Send reply if we are the intended recipient */
+            if(!memcmp(nif->ip_addr, pkt->pr_recv, 4))
+                net_arp_send(nif, pkt);
             __fallthrough; /* Yes, this really should fall through here. */
 
         case 2: /* ARP Reply */
