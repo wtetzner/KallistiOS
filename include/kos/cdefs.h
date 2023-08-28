b19e2dfd133235754b431819fa9a8b812247ec75
Lawrence Sebald <ljsebald@users.sourceforge.net>
2023-08-28 17:05:00 -0400

    Might as well make this work if you ask for c89/c90/ansi-c as well...

--- include/kos/cdefs.h
+++ include/kos/cdefs.h
@@ -117,15 +117,9 @@
 
 /* C++ doesn't support the restrict keyword from C99. As long as we're using
    GCC (or something like it), we can use __restrict__ instead in C++. */
-#ifndef restrict
-#ifdef __cplusplus
-#ifdef __GNUC__
+#if !defined(restrict) && (defined(__cplusplus) || __STDC_VERSION__ < 199901L)
 /** \brief  Identify a C99-style restricted pointer. */
 #define restrict __restrict__
-#else /* __GNUC__ */
-#define restrict
-#endif /* __GNUC__ */
-#endif /* __cplusplus */
-#endif /* !restrict */
+#endif /* !restrict && (__cplusplus || __STDC_VERSION__ < 199901L) */
 
 #endif  /* __KOS_CDEFS_H */
